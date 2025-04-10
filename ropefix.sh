7987a0cb02c90c9839ca451cdb94d24cebc9a5cd
drnatebrown <drnatebrown@gmail.com>
2025-04-10 16:21:17 -0400

    Add script for mending ropebwt3 fragment output

---
+++ ropefix.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Turns output of using fragments with ropebwt3 into a file equivalent to running
+# ropebwt3 without using KeBaB first.
+# Usage: ./ropefix.sh input_file > output_file
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 input_file"
+    echo "input_file is the output of running ropebwt3 with fragments"
+    exit 1
+fi
+
+awk -F'\t' '{
+    split($1, a, /[:-]/)
+    o = a[2] - 1
+    print a[1] "\t" $2 + o "\t" $3 + o "\t" $4
+}' "$1"
