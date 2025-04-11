2889ed495a6b05800c8eb4125fed02300dbe09f3
Nathaniel Brown <drnatebrown@gmail.com>
2025-04-11 00:15:23 -0400

    Small ropefix changes to language

--- ropefix.sh
+++ ropefix.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Turns output of using fragments with ropebwt3 into a file equivalent to running
-# ropebwt3 without using KeBaB first.
+# Modifies output of using fragments with ropebwt3 into a 
+# file equivalent to running ropebwt3 first without KeBaB.
 # Usage: ./ropefix.sh input_file > output_file
 
 if [ $# -ne 1 ]; then
     echo "Usage: $0 [MEM_FILE]"
-    echo "[MEM_FILE] is the output of running ropebwt3 with KeBaBfragments"
+    echo "[MEM_FILE] is the output of running ropebwt3 with KeBaB fragments"
     exit 1
 fi
 
